4580cef004c972f4ae82b9a4f1eb89c0c27697db
David Haglund <github2@davidhaglund.se>
2011-09-28 21:24:46 +0200

    Starting to implement the epw (event processing worker) behaviour.

---
+++ epa/src/epw.erl
@@ -0,0 +1,149 @@
+%%====================================================================
+%% Copyright (c) 2011, David Haglund
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions
+%% are met:
+%%
+%%     * Redistributions of source code must retain the above
+%%       copyright notice, this list of conditions and the following
+%%       disclaimer.
+%%
+%%     * Redistributions in binary form must reproduce the above
+%%       copyright notice, this list of conditions and the following
+%%       disclaimer in the documentation and/or other materials
+%%       provided with the distribution.
+%%
+%%     * Neither the name of the copyright holder nor the names of its
+%%       contributors may be used to endorse or promote products
+%%       derived from this software without specific prior written
+%%       permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+%% FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+%% COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+%% INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+%% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+%% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+%% HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+%% STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+%% OF THE POSSIBILITY OF SUCH DAMAGE.
+%%====================================================================
+-module(epw). % event processing worker
+
+-behaviour(gen_server).
+-define(SERVER, ?MODULE).
+
+%% --------------------------------------------------------------------
+%% Include files
+%% --------------------------------------------------------------------
+
+%% --------------------------------------------------------------------
+%% External exports
+-export([behaviour_info/1]).
+-export([start_link/2]).
+-export([stop/1]).
+
+%% gen_server callbacks
+-export([init/1, handle_call/3, handle_cast/2, handle_info/2, terminate/2, code_change/3]).
+
+-record(state, {
+                callback,
+                user_args
+               }).
+
+%% ====================================================================
+%% External functions
+%% ====================================================================
+behaviour_info(callbacks) ->
+    [{init,1},
+     {execute, 2},
+     {terminate, 2}];
+behaviour_info(_Other) ->
+    undefined.
+
+start_link(Callback, UserArgs) ->
+    gen_server:start_link(?MODULE, #state{callback=Callback, user_args = UserArgs}, []).
+
+stop(Pid) ->
+    gen_server:call(Pid, stop).
+
+%% ====================================================================
+%% Server functions
+%% ====================================================================
+
+%% --------------------------------------------------------------------
+%% Function: init/1
+%% Description: Initiates the server
+%% Returns: {ok, State}          |
+%%          {ok, State, Timeout} |
+%%          ignore               |
+%%          {stop, Reason}
+%% --------------------------------------------------------------------
+init(State) ->
+    Callback = State#state.callback,
+    {ok, UserArgs} = Callback:init(State#state.user_args),
+    {ok, State#state{user_args = UserArgs}}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_call/3
+%% Description: Handling call messages
+%% Returns: {reply, Reply, State}          |
+%%          {reply, Reply, State, Timeout} |
+%%          {noreply, State}               |
+%%          {noreply, State, Timeout}      |
+%%          {stop, Reason, Reply, State}   | (terminate/2 is called)
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_call(stop, _From, State) ->
+    Callback = State#state.callback,
+    UserArgs = Callback:terminate(normal,  State#state.user_args),
+    {stop, normal, {ok, UserArgs}, State};
+handle_call(_Request, _From, State) ->
+    Reply = ok,
+    {reply, Reply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_cast/2
+%% Description: Handling cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_cast(_Msg, State) ->
+    {noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: handle_info/2
+%% Description: Handling all non call/cast messages
+%% Returns: {noreply, State}          |
+%%          {noreply, State, Timeout} |
+%%          {stop, Reason, State}            (terminate/2 is called)
+%% --------------------------------------------------------------------
+handle_info(_Info, State) ->
+    {noreply, State}.
+
+%% --------------------------------------------------------------------
+%% Function: terminate/2
+%% Description: Shutdown the server
+%% Returns: any (ignored by gen_server)
+%% --------------------------------------------------------------------
+terminate(_Reason, _State) ->
+    ok.
+
+%% --------------------------------------------------------------------
+%% Func: code_change/3
+%% Purpose: Convert process state when code is changed
+%% Returns: {ok, NewState}
+%% --------------------------------------------------------------------
+code_change(_OldVsn, State, _Extra) ->
+    {ok, State}.
+
+%% --------------------------------------------------------------------
+%%% Internal functions
+%% --------------------------------------------------------------------
+
---
+++ epa/test/epw_tests.erl
@@ -0,0 +1,112 @@
+%%====================================================================
+%% Copyright (c) 2011, David Haglund
+%% All rights reserved.
+%%
+%% Redistribution and use in source and binary forms, with or without
+%% modification, are permitted provided that the following conditions
+%% are met:
+%%
+%%     * Redistributions of source code must retain the above
+%%       copyright notice, this list of conditions and the following
+%%       disclaimer.
+%%
+%%     * Redistributions in binary form must reproduce the above
+%%       copyright notice, this list of conditions and the following
+%%       disclaimer in the documentation and/or other materials
+%%       provided with the distribution.
+%%
+%%     * Neither the name of the copyright holder nor the names of its
+%%       contributors may be used to endorse or promote products
+%%       derived from this software without specific prior written
+%%       permission.
+%%
+%% THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+%% "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+%% LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS
+%% FOR A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE
+%% COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT,
+%% INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES
+%% (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+%% SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION)
+%% HOWEVER CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT,
+%% STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
+%% ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED
+%% OF THE POSSIBILITY OF SUCH DAMAGE.
+%%====================================================================
+-module(epw_tests).
+
+-ifdef(TEST).
+
+-include_lib("eunit/include/eunit.hrl").
+
+-behaviour(epw).
+-export([init/1, execute/2, terminate/2]).
+
+% callback methods
+init(Pid) when is_pid(Pid) ->
+    Pid ! init,
+    {ok, []};
+init(Args) ->
+    {ok, Args}.
+
+execute(Pid, State) when is_pid(Pid) ->
+    Pid ! {execute, State},
+    {ok, State};
+execute(_Data, State) ->
+    {ok, State}.
+
+terminate(_Reason, List) when is_list(List) ->
+    case proplists:get_value(terminate, List) of
+        Pid when is_pid(Pid) ->
+            Pid ! {stopped, List},
+            terminated;
+        undefined ->
+            not_the_state
+    end;
+terminate(_Reason, State) ->
+    State.
+
+% Tests
+start_stop_test() ->
+    {ok, Pid} = epw:start_link(?MODULE, []),
+    ?assertNot(undefined == process_info(Pid)),
+    {ok, _} = epw:stop(Pid).
+
+init_test() ->
+    Pid = start(),
+    receive init -> ok end,
+    epw:stop(Pid).
+
+stop_should_return_the_state_test() ->
+    Ref = make_ref(),
+    Pid = start(Ref),
+    ?assertEqual({ok, Ref}, epw:stop(Pid)).
+
+stop_should_call_terminate_test() ->
+    Args = [{terminate, self()}],
+    Pid = start(Args),
+    ?assertEqual({ok, terminated}, epw:stop(Pid)),
+    receive {stopped, Args} -> ok end.
+
+behaviour_info_test() ->
+    Expected = [{init,1},
+                {execute, 2},
+                {terminate,2}],
+    Actual = epw_behaviour:behaviour_info(callbacks),
+    ?assertEqual(Expected, Actual).
+
+% Helper functions
+flush() ->
+    receive _Any -> flush()
+    after 0 -> true
+    end.
+
+start() ->
+    start(self()).
+
+start(UserArgs) ->
+    flush(),
+    {ok, Pid} = epw:start_link(?MODULE, UserArgs),
+    Pid.
+
+-endif.
